774692fed687b1f7781749f854e6423acd9c1244
haha <haha@itcast.cn>
2018-12-29 15:34:00 +0800

    day02提交

--- src/main/java/cn/itcast/Demo.java
+++ src/main/java/cn/itcast/Demo.java
@@ -5,5 +5,6 @@ import com.sun.org.apache.xerces.internal.dom.PSVIAttrNSImpl;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("nihao");
+        System.out.println("hahah");
     }
 }
